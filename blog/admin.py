ef9cd3f799461b39ea679df62a161ef86481742b
daniildant <kastiel.www@yandex.ru>
2017-10-09 18:46:02 +0500

    Обновили категории

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 from .models import Post, Category
 # from django.contrib.flatpages.models import FlatPage
+# from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import ugettext_lazy as _
+from markdownx.admin import MarkdownxModelAdmin
 
-admin.site.register(Post),
-admin.site.register(Category),
+# admin.site.register(Post),
 
 # Re-register FlatPageAdmin
 # admin.site.unregister(FlatPage)
+
+
+
+# Define a new FlatPageAdmin
+class FlatPageAdmin(MarkdownxModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'markdown', 'category', 'thumbnail')}),
+        (_('Advanced options'), {
+            'classes': ('collapse', ),
+            'fields': (
+                'enable_comments',
+                'registration_required',
+                'template_name',
+                'content',
+                'sites',
+                'seo',
+            ),
+        }),
+    )
+
+# Re-register FlatPageAdmin
+# admin.site.unregister(FlatPage)
+admin.site.register(Post, FlatPageAdmin)
+admin.site.register(Category),
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.list import ListView
+from .models import Category
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', ListView.as_view(model=Category, template_name='blog/post_list.html')),
     # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
